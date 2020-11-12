c26d2edd4947c1bd32b67daf97dc518c4855ddf7
bagohart <bagohart@gmx.de>
2020-11-12 16:26:03 +0100

    scotty hit-counter exercise in ch.26 completed

--- ch26/scotty-hit-counter/app/Main.hs
+++ ch26/scotty-hit-counter/app/Main.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Data.IORef
@@ -11,6 +13,7 @@ import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as TL
 import System.Environment (getArgs)
 import Web.Scotty.Trans
+-- import Web.Scotty -- conflicts with the Trans thing above.
 
 data Config = Config {
     counts :: IORef (M.Map Text Integer)
@@ -19,23 +22,35 @@ data Config = Config {
 
 type Scotty = ScottyT Text (ReaderT Config IO)
 type Handler = ActionT Text (ReaderT Config IO)
+
 bumpBoomp :: Text -> M.Map Text Integer -> (M.Map Text Integer, Integer)
-bumpBoomp k m = undefined
+bumpBoomp k m = (newMap, newVal)
+    where newVal = 1 + M.findWithDefault 0 k m -- or fromMaybe 0 $ lookup k m
+          newMap = M.insert k newVal m
 
 app :: Scotty ()
 app = get "/:key" $ do
-        unprefixed <- param "key"
-        let key' = mappend undefined unprefixed
-        newInteger <- undefined
-        html $ mconcat [ "<h1>Success! Count was: "
-                        , TL.pack $ show newInteger
-                        , "</h1>"
-                        ]
+    unprefixed <- param "key"
+    prefixx <- lift $ asks prefix -- there is a magic implicit ReaderT here, which we can use with even more magic :')
+    let key :: Text
+        key = unprefixed
+        key' = mappend prefixx unprefixed 
+    countoor <- lift $ asks counts
+    liftIO $ modifyIORef' countoor (fst . (bumpBoomp key'))
+    newInteger <- liftIO (M.lookup key' <$> readIORef countoor)
+    html $ mconcat [ "<h1>Success! Count was: "
+                    , TL.pack $ show newInteger
+                    , "</h1>"
+                    ]
 
 main :: IO ()
 main = do
-    [prefixArg] <- getArgs
+    args <- getArgs
+    if length args /= 1 then (error "Pass exactly one argument, please.") else return ()
+    putStrLn ("Arguments: " ++ show args)
     counter <- newIORef M.empty
-    let config = undefined
-        runR = undefined
+    let config = Config counter (TL.pack $ head args)
+        -- runR :: Int -- use this to find out type of runR, or just look veeeeeery closely at the types
+        runR m = runReaderT m config -- this design is probably what was actually expected in the exercise in ch. 22 o_O
     scottyT 3000 runR app
+
