5c2c55a4317d112215ac5840e77a26091c7aff26
Bagohart <bagohart@gmx.de>
2026-02-20 13:36:21 +0100

    ch30: exceptions. again.

---
+++ ch30_some_years_later/write_pls.hs
@@ -0,0 +1,15 @@
+-- writePls.hs
+
+module Main where
+
+import Control.Exception
+import Data.Typeable
+
+handler :: SomeException -> IO ()
+handler (SomeException e) = do
+    print (typeOf e)
+    putStrLn ("We errored! It was: " ++ show e)
+
+main =
+    writeFile "zzz" "hi"
+        `catch` handler
